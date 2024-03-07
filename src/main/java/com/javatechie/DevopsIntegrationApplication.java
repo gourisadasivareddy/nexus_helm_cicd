f5caf12491988f3e651695d64cf356cd95a93a1d
gourisadasivareddy <sada.siva833@gmail.com>
2024-03-07 02:22:01 +0000

    changes for pipline

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to Mr. DevOps Youtube Channel";
+		return "welcome to My Advance practice project";
 	}
 
 	public static void main(String[] args) {
